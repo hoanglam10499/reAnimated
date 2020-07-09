5f0488e8b78b2c9dc8dd880cdfae9b7377aabbb3
Hoang Lam <hoanglam10499@gmail.com>
2020-07-09 17:54:27 +0700

    Update airbnbHeader.js

--- airbnbHeader.js
+++ airbnbHeader.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import {
   StyleSheet,
   View,
@@ -7,21 +7,21 @@ import {
   StatusBar,
   FlatList,
   Platform,
-} from "react-native";
-import Animated from "react-native-reanimated";
-import { withTransition } from "react-native-redash";
+} from 'react-native';
+import Animated from 'react-native-reanimated';
+import {withTransition} from 'react-native-redash';
 
-const { width, height } = Dimensions.get("window");
+const {width, height} = Dimensions.get('window');
 const FlatListAnim = Animated.createAnimatedComponent(FlatList);
 
 const heightStatus = StatusBar.currentHeight;
-const HEIGHT_HEADER = Platform.OS === "ios" ? 115 : 70 + heightStatus;
+const HEIGHT_HEADER = Platform.OS === 'ios' ? 115 : 70 + heightStatus;
 
 const index = () => {
-  const data = [{ color: "red" }, { color: "yellow" }, { color: "blue" }];
+  const data = [{color: 'red'}, {color: 'yellow'}, {color: 'blue'}];
   const y = new Animated.Value(0);
   const diffClamp = Animated.diffClamp(y, 0, HEIGHT_HEADER);
-  const onScroll = Animated.event([{ nativeEvent: { contentOffset: { y } } }]);
+  const onScroll = Animated.event([{nativeEvent: {contentOffset: {y}}}]);
   const transition = withTransition(diffClamp);
   const translateY = Animated.interpolate(transition, {
     inputRange: [0, HEIGHT_HEADER],
@@ -32,21 +32,29 @@ const index = () => {
       <Animated.View
         style={[
           styles.header,
-          { height: HEIGHT_HEADER, transform: [{ translateY }] },
-        ]}
-      >
+          {height: HEIGHT_HEADER, transform: [{translateY}]},
+        ]}>
         <Text>Header</Text>
       </Animated.View>
       <FlatListAnim
-        {...{ onScroll }}
+        {...{onScroll}}
         data={data}
         bounces={false}
         scrollEventThrottle={16}
         showsVerticalScrollIndicator={false}
         contentContainerStyle={styles.list}
-        keyExtractor={(item, index) => "List-" + index}
-        renderItem={({ item, index }) => {
-          return <View style={styles.listItem(item.color)} />;
+        keyExtractor={(item, index) => 'List-' + index}
+        renderItem={() => {
+          return (
+            <View
+              style={{
+                width: width / 1.2,
+                height: height / 1.2,
+                backgroundColor,
+                marginBottom: 20,
+              }}
+            />
+          );
         }}
       />
     </View>
@@ -60,20 +68,14 @@ const styles = StyleSheet.create({
   },
   header: {
     ...StyleSheet.absoluteFill,
-    backgroundColor: "grey",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: 'grey',
+    alignItems: 'center',
+    justifyContent: 'center',
     zIndex: 1,
     elevation: 1,
   },
   list: {
-    alignItems: "center",
+    alignItems: 'center',
     paddingTop: HEIGHT_HEADER + 10,
   },
-  listItem: (backgroundColor) => ({
-    width: width / 1.2,
-    height: height / 1.2,
-    backgroundColor,
-    marginBottom: 20,
-  }),
 });
